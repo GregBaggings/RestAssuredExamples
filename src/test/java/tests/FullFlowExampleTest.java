88a7178f9570d71e7bd8830dde92784977433061
Gergely Agnecz <agnegerg@gmail.com>
2019-03-27 23:53:57 +0100

    Added new example how to use a POJO to map a response for more accurate data validation

--- src/test/java/tests/FullFlowExampleTest.java
+++ src/test/java/tests/FullFlowExampleTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.git.models.User;
 import io.git.storage.BodyStorage;
 import io.restassured.response.Response;
 import org.apache.commons.io.FileUtils;
@@ -9,6 +10,7 @@ import java.io.File;
 import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class FullFlowExampleTest {
 
@@ -34,10 +36,15 @@ public class FullFlowExampleTest {
                 .get("http://petstore.swagger.io/v2/user/{username}");
         System.out.println("The user details are: " + getUser.asString() + "\n");
 
+        // Step2. - Get back the user details more professional
+        User userDetails = given().log().all().header(ACCEPT_HEADER, APPLICATION_JSON).and().pathParam("username", userName).when()
+                .get("http://petstore.swagger.io/v2/user/{username}").as(User.class);
+        System.out.println("The user details are: " + userDetails.toString() + "\n");
+        assertEquals("Username was not correct.", "JL", userDetails.getUsername());
+
         // Step3. - Delete the user
-        Response deleteUser = given().log().all().pathParam("username", userName).when().delete("http://petstore.swagger.io/v2/user/{username}");
-        System.out.println("The user details are: " + deleteUser.asString());
-        System.out.println("User delete: Done." + "\n");
+        given().log().all().pathParam("username", userName).when().delete("http://petstore.swagger.io/v2/user/{username}");
+        System.out.println("User delete: Done.");
 
         // Step4. - Check if the user is really deleted from the DB
         Response getUserAfterDelete = given().log().all().header("Accept", "application/json").and().pathParam("username", userName).and().expect()
@@ -45,4 +52,5 @@ public class FullFlowExampleTest {
         System.out.println("The user details are: " + getUserAfterDelete.asString());
         System.out.println("User is really deleted.");
     }
+
 }
